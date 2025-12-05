e705e06f9a6c575370ce7313e805281b8d27bdce
Diego Alejandro <alexgonzalezgaytan@gmail.com>
2025-12-05 00:38:28 -0600

    Proyecto inicial

---
+++ src/main/java/com/edufinanciero/edufinanciero/EdufinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.edufinanciero.edufinanciero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufinancieroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edufinanciero/edufinanciero/EdufinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufinanciero.edufinanciero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
