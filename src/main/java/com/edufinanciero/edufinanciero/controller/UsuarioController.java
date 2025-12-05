27f8e744c6b6587082e3173fc7f84eea73383f23
Diego Alejandro <alexgonzalezgaytan@gmail.com>
2025-12-05 13:30:13 -0600

    Anotaciones importantes en UsuarioController.java,  UsuarioDao.java y configuracion de BD en application.properties

--- src/main/java/com/edufinanciero/edufinanciero/controller/UsuarioController.java
+++ src/main/java/com/edufinanciero/edufinanciero/controller/UsuarioController.java
@@ -3,10 +3,12 @@ package com.edufinanciero.edufinanciero.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.edufinanciero.edufinanciero.dao.UsuarioDao;
 import com.edufinanciero.edufinanciero.model.Usuario;
 
+@RestController
 public class UsuarioController {
     private UsuarioDao usuarioDao;;
 
--- src/main/java/com/edufinanciero/edufinanciero/dao/UsuarioDao.java
+++ src/main/java/com/edufinanciero/edufinanciero/dao/UsuarioDao.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
 import com.edufinanciero.edufinanciero.model.Usuario;
 
+@Service
 public class UsuarioDao {
     private JdbcTemplate jdbcTemplate;
 
